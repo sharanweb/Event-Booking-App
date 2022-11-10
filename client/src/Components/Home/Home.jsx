6bb189a55898f6cc3283501df4da725bcb3f56a8
Sharan Dooganavar <91963829+sharanweb@users.noreply.github.com>
2022-11-10 11:50:26 +0530

    home update

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -3,12 +3,23 @@ import Container from "@mui/material/Container";
 import { Box, Button, Divider } from "@mui/material";
 import { Calender } from "../Calender/calender";
 import Link from "@mui/material/Link";
+import { Typography } from '@mui/material';
 
 export const Home = () => {
   return (
     <div className="homemainpage">
+      <Box sx={{ height: 40, mt:0.7, backgroundColor:"rgb(251, 180, 84)", color:"rgb(51, 48, 228)" }}>
+
+      </Box>
       <Container>
-        <Box sx={{ mt: 2 }}>
+        <Box
+          sx={{
+            mt: 2,
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
           <Box
             sx={{
               borderRadius: 2,
@@ -21,10 +32,29 @@ export const Home = () => {
               boxShadow: 3,
             }}
           >
-            <Box sx={{ textAlign: "center", display:"flex", justifyContent:"space-around" }}>
+            <Box
+              sx={{
+                textAlign: "center",
+                display: "flex",
+                justifyContent: "space-around",
+              }}
+            >
               <Link href="/book">
-                <Box sx={{ width: 150, height: 50, padding: 1, display:"flex", justifyContent:"space-around", backgroundColor:"rgb(217, 248, 196)" , borderRadius:2}}>
-                  <img src="https://cdn-icons-png.flaticon.com/512/4206/4206324.png" className="buttonimage"></img>
+                <Box
+                  sx={{
+                    width: 150,
+                    height: 50,
+                    padding: 1,
+                    display: "flex",
+                    justifyContent: "space-around",
+                    backgroundColor: "rgb(217, 248, 196)",
+                    borderRadius: 2,
+                  }}
+                >
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/4206/4206324.png"
+                    className="buttonimage"
+                  ></img>
                   <h2 className="createname">Create</h2>
                 </Box>
               </Link>
@@ -34,7 +64,12 @@ export const Home = () => {
               <Calender></Calender>
             </Box>
           </Box>
-          <Box></Box>
+          <Box sx={{ width: 700, height: 430 }}>
+            <img
+              src="https://img.freepik.com/free-vector/appointment-booking-with-man-checking-calendar_23-2148558747.jpg?w=2000"
+              className="homedivwelcome"
+            ></img>
+          </Box>
         </Box>
       </Container>
     </div>
