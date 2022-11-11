caab1d37c27ab9bf45d48e405173c2cb89bb7e53
Sharan Dooganavar <91963829+sharanweb@users.noreply.github.com>
2022-11-11 12:52:17 +0530

    ui update

--- client/src/Components/Book_modal/book_card.jsx
+++ client/src/Components/Book_modal/book_card.jsx
@@ -22,6 +22,7 @@ import { blue } from "@mui/material/colors";
 import { useState, useEffect, useContext } from "react";
 import { DateContext } from "../Context/date.context";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import Divider from "@mui/material/Divider";
 import Chip from "@mui/material/Chip";
 
@@ -54,6 +55,7 @@ export const BookModal = () => {
     guests: [],
     description: "",
   });
+  const navigate = useNavigate();
 
   const theme = useTheme();
 
@@ -68,12 +70,12 @@ export const BookModal = () => {
   };
 
   //get the host from the local storage
-  let hostDetail
+  let hostDetail;
   let hostin = JSON.parse(localStorage.getItem("username"));
-  if(hostin != null){
+  if (hostin != null) {
     hostDetail = JSON.parse(localStorage.getItem("username")).user._id;
   }
-  
+
   //console.log(hostDetail);
 
   //storing the names of multiple select into an array
@@ -117,7 +119,10 @@ export const BookModal = () => {
     console.log("clicked");
     axios
       .post(`https://book2meet.herokuapp.com/event/create`, formdata)
-      .then((res) => console.log(res.data))
+      .then((res) => {
+        alert("Event Created Successfully");
+        navigate("/meetings");
+      })
       .catch((res) => console.log("eoor", res));
   };
 
@@ -126,160 +131,146 @@ export const BookModal = () => {
   }, []);
 
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Container>
+    <div className="bookback">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+        }}
+      >
+        <Box
+          sx={{
+            width: 300,
+            height: 450,
+            mt: 2,
+            backgroundColor: "white",
+            padding: 2,
+            borderColor: "black",
+            boxShadow: 3,
+            borderRadius: "16px",
+            alignItems: "center",
+            zIndex:5,
+            opacity:3
+            
+          }}
+        >
+          <h1 className="title_of">Create Event</h1>
           <Box
+            component="form"
+            noValidate
             sx={{
+              mt: 1.5,
               display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-around",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              height: 400,
             }}
           >
-            <Box
-              sx={{
-                width: 300,
-                height: 450,
-                mt: 2,
-                backgroundColor: blue[50],
-                padding: 2,
-                borderColor: "black",
-                boxShadow: 3,
-                borderRadius: "16px",
-                alignItems: "center",
-                "&:hover": {
-                  opacity: [0.9, 0.8, 0.7],
-                },
-              }}
-            >
-              <h1 className="title_of">Create Event</h1>
-              <Box
-                component="form"
-                noValidate
-                sx={{
-                  mt: 1.5,
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-between",
-                  height: 400,
-                }}
-              >
-                <TextField
-                  id="outlined-basic"
-                  label="Event name"
-                  variant="outlined"
-                  name="name"
-                  value={formdata.name}
-                  onChange={handleTextChange}
-                  sx={{ width: 300 }}
-                  size="small"
-                  fullWidth
-                ></TextField>
-                <Box>
-                  <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker
-                      label="Date"
-                      value={formdata.date}
-                      name="date"
-                      onChange={handleDateChange}
-                      renderInput={(params) => (
-                        <TextField
-                          {...params}
-                          size="small"
-                          sx={{ width: 300 }}
-                        />
-                      )}
-                    />
-                  </LocalizationProvider>
-                </Box>
-                <FormControlLabel
-                  control={<Checkbox sx={{ color: "grey[500]" }} />}
-                  label="Recurring Meeting?"
+            <TextField
+              id="outlined-basic"
+              label="Event name"
+              variant="outlined"
+              name="name"
+              value={formdata.name}
+              onChange={handleTextChange}
+              sx={{ width: 300 }}
+              size="small"
+              fullWidth
+            ></TextField>
+            <Box>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Date"
+                  value={formdata.date}
+                  name="date"
+                  onChange={handleDateChange}
+                  renderInput={(params) => (
+                    <TextField {...params} size="small" sx={{ width: 300 }} />
+                  )}
                 />
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <TimePicker
-                      label="From"
-                      value={formdata.fromtime}
-                      name="fromtime"
-                      // onChange={(newValue) => {
-                      //   //console.log(newValue.$d.toLocaleString())
-                      //   setFrom(newValue.$d.toLocaleString());
-                      // }}
-                      onChange={handleFromTimeChange}
-                      // onChange={handleFromChange}
-                      renderInput={(params) => (
-                        <TextField {...params} size="small" />
-                      )}
-                    />
-                  </LocalizationProvider>
-                  <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <TimePicker
-                      label="To"
-                      height="10"
-                      size="small"
-                      value={formdata.totime}
-                      name="totime"
-                      // onChange={(newValue) => {
-                      //   setTo(newValue.$d.toLocaleString());
-                      // }}
-                      onChange={handleToTimeChange}
-                      renderInput={(params) => (
-                        <TextField {...params} size="small" />
-                      )}
-                    />
-                  </LocalizationProvider>
-                </Box>
-                <Box>
-                  <FormControl sx={{ width: 300 }}>
-                    <InputLabel id="demo-multiple-name-label">
-                      Guests
-                    </InputLabel>
-                    <Select
-                      labelId="demo-multiple-name-label"
-                      id="demo-multiple-name"
-                      multiple
-                      value={formdata.guests}
-                      name="guests"
-                      onChange={handleChange}
-                      input={<OutlinedInput label="Name" />}
-                      MenuProps={MenuProps}
-                    >
-                      {userData.map((el) => (
-                        <MenuItem key={el._id} value={el._id}>
-                          {el.firstName} {el.lastName}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </Box>
-                <Box>
-                  <TextField
-                    id="outlined-multiline-static"
-                    label="Description"
-                    value={formdata.description}
-                    name="description"
-                    multiline
-                    onChange={handleTextChange}
-                    sx={{ width: 300 }}
-                    rows={3}
-                  />
-                </Box>
-                <Button variant="contained" onClick={handleClick}>
-                  Create
-                </Button>
-              </Box>
+              </LocalizationProvider>
             </Box>
             
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <TimePicker
+                  label="From"
+                  value={formdata.fromtime}
+                  name="fromtime"
+                  // onChange={(newValue) => {
+                  //   //console.log(newValue.$d.toLocaleString())
+                  //   setFrom(newValue.$d.toLocaleString());
+                  // }}
+                  onChange={handleFromTimeChange}
+                  // onChange={handleFromChange}
+                  renderInput={(params) => (
+                    <TextField {...params} size="small" />
+                  )}
+                />
+              </LocalizationProvider>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <TimePicker
+                  label="To"
+                  height="10"
+                  size="small"
+                  value={formdata.totime}
+                  name="totime"
+                  // onChange={(newValue) => {
+                  //   setTo(newValue.$d.toLocaleString());
+                  // }}
+                  onChange={handleToTimeChange}
+                  renderInput={(params) => (
+                    <TextField {...params} size="small" />
+                  )}
+                />
+              </LocalizationProvider>
+            </Box>
+            <Box>
+              <FormControl sx={{ width: 300 }}>
+                <InputLabel id="demo-multiple-name-label">Guests</InputLabel>
+                <Select
+                  labelId="demo-multiple-name-label"
+                  id="demo-multiple-name"
+                  multiple
+                  value={formdata.guests}
+                  name="guests"
+                  onChange={handleChange}
+                  input={<OutlinedInput label="Name" />}
+                  MenuProps={MenuProps}
+                >
+                  {userData.map((el) => (
+                    <MenuItem key={el._id} value={el._id}>
+                      {el.firstName} {el.lastName}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+            <Box>
+              <TextField
+                id="outlined-multiline-static"
+                label="Description"
+                value={formdata.description}
+                name="description"
+                multiline
+                onChange={handleTextChange}
+                sx={{ width: 300 }}
+                rows={3}
+              />
+            </Box>
+            <Button variant="contained" onClick={handleClick}>
+              Create
+            </Button>
           </Box>
-        </Container>
-      </ThemeProvider>
-    </>
+        </Box>
+      </Box>
+    </div>
   );
 };
